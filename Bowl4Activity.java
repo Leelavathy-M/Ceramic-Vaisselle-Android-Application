d0a21a7dc39d0ad7ea92f874d42d0fa8c9bc3b9c
LEELAVATHY M <157588125+Leelavathy-M@users.noreply.github.com>
2024-04-18 20:44:26 +0530

    Create Bowl4Activity.java

---
+++ Bowl4Activity.java
@@ -0,0 +1,27 @@
+package com.example.ceramicvessille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Bowl4Activity extends AppCompatActivity {
+
+    Button PaymentBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bowl4);
+
+        PaymentBtn =findViewById(R.id.btn_bowl4);
+
+        PaymentBtn.setOnClickListener(view -> {
+            Intent intent=new Intent(Bowl4Activity.this,PaymentActivity.class);
+            startActivity(intent);
+        });
+    }
+}

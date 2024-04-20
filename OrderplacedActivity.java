27d8e65dda1eb12a8f5e1093d776c44100b9c92b
LEELAVATHY M <157588125+Leelavathy-M@users.noreply.github.com>
2024-04-20 07:58:42 +0530

    Create OrderplacedActivity.java

---
+++ OrderplacedActivity.java
@@ -0,0 +1,14 @@
+package com.example.ceramicvessille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderplacedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orderplaced);
+    }
+}

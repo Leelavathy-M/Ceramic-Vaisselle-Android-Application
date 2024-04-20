6de8e0eb60872bdc8023d2947858f7dd6e78b5c5
LEELAVATHY M <157588125+Leelavathy-M@users.noreply.github.com>
2024-04-20 08:04:28 +0530

    Create Spoon5Activity.java

---
+++ Spoon5Activity.java
@@ -0,0 +1,27 @@
+package com.example.ceramicvessille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Spoon5Activity extends AppCompatActivity {
+
+
+    Button PaymentBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spoon5);
+
+        PaymentBtn =findViewById(R.id.btn_spoon5);
+
+        PaymentBtn.setOnClickListener(view -> {
+            Intent intent=new Intent(Spoon5Activity.this,PaymentActivity.class);
+            startActivity(intent);
+        });
+    }
+}
